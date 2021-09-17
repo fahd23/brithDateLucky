07471ed12d149f55c1b662e6656f0c7fa707254c
fahd23 <fahdpno@gmail.com>
2021-09-17 16:35:53 +0530

    update: fix -ve input

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ function validate() {
     outputmsg.style.display = "none";
     const dob = birthDate.value;
     const sum = calculateSum(dob);
-    if(sum&&luckyNumber.value)
+    if(sum&&luckyNumber.value > 0)
     compareValues(sum,luckyNumber.value);
-    else
-    msg("Please enter both the fields");
+    else if(luckyNumber.value <= 0){
+        msg("Please enter Lucky number as positive");
+    }else{
+        msg("Please enter both the fields");
+    }
+
 }
 
 function msg(text) {
